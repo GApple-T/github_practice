b349bbd2d083e7b9dbcee3efac89b60aaf4fd1c3
한재형 <wjdtjffl55@naver.com>
2023-08-21 19:26:47 +0900

    상단부 구현

--- Memo/ContentView.swift
+++ Memo/ContentView.swift
@@ -8,14 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var search = ""
     var body: some View {
-        VStack {
-            Image(systemName:"globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hellod!")
+        ZStack{
+            Color.black.ignoresSafeArea()
+            ScrollView{
+                VStack{
+                    HStack{
+                        Spacer()
+                        Button{
+                            
+                        }label: {
+                            Text("편집")
+                                .foregroundColor(.orange)
+                        }
+                    }.padding(.horizontal)
+                    HStack{
+                        Text("폴더")
+                            .foregroundColor(.white)
+                            .fontWeight(.bold)
+                            .font(.system(size: 35))
+                        Spacer()
+                    }.padding(.horizontal)
+                    TextField("  검색", text: $search)
+                        .foregroundColor(.white)
+                        .frame(width: 365, height: 35)
+                        .background(Color("back"))
+                        .cornerRadius(8)
+                }
+            }
         }
-        .padding()
     }
 }
 
