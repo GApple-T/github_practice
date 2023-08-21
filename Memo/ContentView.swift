a0db017531bcc9045530a9dd37c3ec02ac1dce09
한재형 <wjdtjffl55@naver.com>
2023-08-21 23:00:09 +0900

    최종완성

--- Memo/ContentView.swift
+++ Memo/ContentView.swift
@@ -10,66 +10,68 @@ import SwiftUI
 struct ContentView: View {
     @State private var search = ""
     var body: some View {
-        ZStack{
-            Color.black.ignoresSafeArea()
-            VStack{
-                HStack{
-                    Spacer()
-                    Button{
-                        //
-                    }label: {
-                        Text("편집")
-                            .foregroundColor(.orange)
-                    }
-                }.padding(.horizontal)
-                HStack{
-                    Text("폴더")
+        NavigationStack{
+            ZStack{
+                Color.black.ignoresSafeArea()
+                VStack{
+                    HStack{
+                        Spacer()
+                        Button{
+                            //
+                        }label: {
+                            Text("편집")
+                                .foregroundColor(.orange)
+                        }
+                    }.padding(.horizontal)
+                    HStack{
+                        Text("폴더")
+                            .foregroundColor(.white)
+                            .fontWeight(.bold)
+                            .font(.system(size: 35))
+                        Spacer()
+                    }.padding(.horizontal)
+                    TextField("  검색", text: $search)
                         .foregroundColor(.white)
-                        .fontWeight(.bold)
-                        .font(.system(size: 35))
-                    Spacer()
-                }.padding(.horizontal)
-                TextField("  검색", text: $search)
-                    .foregroundColor(.white)
-                    .frame(width: 365, height: 35)
-                    .background(Color("back"))
-                    .cornerRadius(8)
-                List{
-                    Section{
-                        HStack{
-                            Image(systemName: "folder")
-                                .foregroundColor(.orange)
-                            Spacer()
-                            Text("모든 iCloud")
-                            Image(systemName: "chevron.right")
-                                .foregroundColor(Color(.systemGray))
+                        .frame(width: 365, height: 35)
+                        .background(Color("back"))
+                        .cornerRadius(8)
+                    List{
+                        Section{
+                            NavigationLink(destination: iCloudMemo()){
+                                HStack{
+                                    Image(systemName: "folder")
+                                        .foregroundColor(.orange)
+                                    Spacer()
+                                    Text("모든 iCloud")
+                                }
+                            }
+                            NavigationLink(destination: Memo()){
+                                HStack{
+                                    Image(systemName: "folder")
+                                        .foregroundColor(.orange)
+                                    Spacer()
+                                    Text("메모")
+                                }
+                            }
+                            NavigationLink(destination: DreamMemo()){
+                                HStack{
+                                    Image(systemName: "folder")
+                                        .foregroundColor(.orange)
+                                    Spacer()
+                                    Text("내 꿈")
+                                }
+                            }
+                        } header: {
+                            Text("iCloud")
                         }
-                        HStack{
-                            Image(systemName: "folder")
-                                .foregroundColor(.orange)
-                            Spacer()
-                            Text("메모")
-                            Image(systemName: "chevron.right")
-                                .foregroundColor(Color(.systemGray))
-                        }
-                        HStack{
-                            Image(systemName: "folder")
-                                .foregroundColor(.orange)
-                            Spacer()
-                            Text("내 꿈")
-                            Image(systemName: "chevron.right")
-                                .foregroundColor(Color(.systemGray))
-                        }
-                    } header: {
-                        Text("iCloud")
+                        .listRowBackground(Color("back"))
+                        .foregroundColor(.white)
+                        .headerProminence(.increased)
                     }
-                    .listRowBackground(Color("back"))
-                    .foregroundColor(.white)
-                    .headerProminence(.increased)
+                    .frame(width: 395)
+                    .background(.black)
+                    .scrollContentBackground(.hidden)
                 }
-                .frame(width: 395)
-                .background(.black)
-                .scrollContentBackground(.hidden)
             }
         }
     }
---
+++ Memo/Memo.swift
@@ -0,0 +1,20 @@
+//
+//  Memo.swift
+//  Memo
+//
+//  Created by 한재형 on 2023/08/21.
+//
+
+import SwiftUI
+
+struct Memo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Memo_Previews: PreviewProvider {
+    static var previews: some View {
+        Memo()
+    }
+}
