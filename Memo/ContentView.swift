5de9d9f890058c5f406de35cc656569799b8cf2a
한재형 <wjdtjffl55@naver.com>
2023-08-21 17:52:32 +0900

    초기설정

---
+++ Memo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Memo
+//
+//  Created by 한재형 on 2023/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, worlasdfd!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
