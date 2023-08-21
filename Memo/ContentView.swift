3e7b0c23179cab5bb53be0799631b2df9265058e
한재형 <wjdtjffl55@naver.com>
2023-08-21 18:05:38 +0900

    줄바꿈 수정

--- Memo/ContentView.swift
+++ Memo/ContentView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {Image(systemName: "globe").imageScale(.large).foregroundColor(.accentColor)
+        VStack {
+            Image(systemName:"globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
             Text("Hellod!")
         }
         .padding()
